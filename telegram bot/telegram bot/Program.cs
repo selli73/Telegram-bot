7eb79e09352093afb23cc848e0319aaaa9839a80
Bulat <aabulath@yandex.ru>
2025-03-29 19:30:51 +0400

    создал метод update и прописал не много локиги

---
+++ telegram bot/telegram bot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection.Metadata;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new TelegramBotClient("7323782581:AAHDVZlRjDDib8jFL9DjaUYLiCoS-M0_RR4");
+        client.StartReceiving(Update, Error);
+
+        Console.ReadLine();
+    }
+
+    async static Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+        var message = update.Message;  // это контент
+        if (message.Text != null)
+        {
+            Console.WriteLine($"{message.Chat.FirstName}     |      {message.Text}");
+            if (message.Text.ToLower().Contains("здорово"))
+            {
+                await client.SendMessage(message.Chat.Id, "Приветик :)");
+                return;
+            }
+        }
+        
+        if (message.Photo != null)
+        {
+            await client.SendMessage(message.Chat.Id, "Классная фотка!");
+            return;
+        }
+    }
+
+
+    private static async Task Error(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
